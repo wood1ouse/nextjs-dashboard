1194ad5cfadec7d18b0f93ef68826b769e75f61b
Maxim Mamontov <mamontov.maxim@lll.kpi.ua>
2025-05-04 22:08:10 +0300

    chapters 1-6

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function Page(): ReactNode {
+    return <p>Invoices Page</p>
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function Page(): ReactNode {
+    return <p>Dashboard Page</p>
+}
